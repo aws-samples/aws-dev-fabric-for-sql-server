a65ab99e67fb5eae281bb47946e8ca41b2fa7b57
marciogmorales <marciogmorales@hotmail.com>
2020-09-01 13:59:14 -0500

    Control Plane upload

---
+++ control-plane/custom-resources/src/subnets_checker.py
@@ -0,0 +1,124 @@
+import boto3
+import cfnresponse
+
+def error_and_exit(error):
+
+    print(error)
+    raise Exception(error)
+
+def handler(event, context):
+
+    try:
+
+        the_event = event['RequestType']
+        response_data = {}
+        phy_res_id = str(hash(event['StackId']
+                              + event['LogicalResourceId']))[1:]
+
+        print("Event dump: %s" % str(event))
+        print("Request type: ", str(the_event))
+
+        if the_event in ('Create', 'Update'):
+
+            input_subnet_list = event['ResourceProperties']['Subnets']
+            the_vpc = event['ResourceProperties']['Vpc']
+            input_count = event['ResourceProperties']['Input']
+            print("Processing subnets: %s." % input_subnet_list)
+
+            ec2 = boto3.client('ec2')
+
+            if len(input_subnet_list) != int(input_count):
+
+                error_and_exit("The amount of seleced subnets and the 'SelectedPrivateSubnetsCont' value differs.")
+
+            print("Validating subnets and VPC ...")
+            response = ec2.describe_subnets(Filters=[
+                {
+                    'Name': 'vpc-id',
+                    'Values': [
+                        the_vpc,
+                    ]
+                },
+            ])
+
+            vpc_subnets = list(map(lambda subnet:
+                                   subnet['SubnetId'],
+                                   response['Subnets']))
+
+            if not all(subnet in vpc_subnets
+                       for subnet in input_subnet_list):
+
+                error_and_exit("All the subnets must belong to the "
+                               + "same VPC [%s]." % the_vpc)
+
+            print("[OK] Passed!")
+
+            print("Validating subnets and AZs ...")
+            az_s = []
+            for sub in response['Subnets']:
+
+                if sub['SubnetId'] in input_subnet_list:
+
+                    if sub['AvailabilityZone'] not in az_s:
+
+                        az_s.append(sub['AvailabilityZone'])
+
+                    else:
+
+                        error_and_exit("Only '1' Subnet per Availability Zone is allowed. You have at least (2) in AZ [%s]." % sub['AvailabilityZone'])
+
+            print("[OK] Passed!")
+
+            print("Validating that subnets are private ...")
+            response = ec2.describe_route_tables(
+                Filters=[
+                    {
+                        'Name': 'association.subnet-id',
+                        'Values': input_subnet_list
+                    },
+                ])
+
+            route_table_list = list(map(lambda route:
+                                        route['Routes'],
+                                        response['RouteTables']))
+
+            for route in [r for sub_list in route_table_list
+                          for r in sub_list]:
+
+                gw_id = route.get('GatewayId', None)
+                if gw_id and gw_id.startswith('igw-'):
+
+                    error_and_exit("This setup does not allow public subnets. Please select only private subnets.")
+
+            print("[OK] Passed!")
+
+            print("Getting VPC CIDR ...")
+            response_data['vpc_cidr'] = ec2.describe_vpcs(Filters=[
+                {
+                    'Name': 'vpc-id',
+                    'Values': [
+                        the_vpc,
+                    ]
+                },
+            ])['Vpcs'][0]['CidrBlock']
+
+            print("CIDR is: %s" % response_data['vpc_cidr'])
+
+        print("Execution succesfull!")
+        cfnresponse.send(event,
+                         context,
+                         cfnresponse.SUCCESS,
+                         response_data,
+                         physicalResourceId=phy_res_id)
+
+    except Exception as e:
+
+        print("Execution failed...")
+        e_string = str(e)
+        print(e_string)
+        context.log_stream_name = e_string
+        cfnresponse.send(event,
+                         context,
+                         cfnresponse.FAILED,
+                         response_data,
+                         physicalResourceId=phy_res_id)
---
+++ control-plane/macro-transformer/src/index.py
@@ -0,0 +1,146 @@
+"""
+## Macro transform ##
+
+Creates N copies of CloudFormation resources, parametrising
+them using a generated random ID.
+
+Based on this AWS Sample macro: https://github.com/awslabs/aws-cloudformation-templates/tree/master/aws/services/CloudFormation/MacrosExamples/Count
+
+"""
+import copy
+import json
+from random import choice
+from string import ascii_lowercase
+
+
+def process_template(template, parameters):
+    """Process the CloudFormation template.
+
+    Parameters
+    ----------
+    template : JSON
+        Input template that is going to be transformed.
+    parameters : list
+        Additional parameters for the processing.
+
+    Returns
+    -------
+    type
+        status (success|failed), processed template
+
+    """
+    new_template = copy.deepcopy(template)
+    status = 'success'
+
+    # Iterate over each of the CloudFormation created resources
+    for name, resource in template['Resources'].items():
+
+        # Check whether the current resource has the 'count' property
+        if 'Count' in resource:
+
+            try:
+                ref_value = new_template['Resources'][name]['Count'].pop('Ref')
+                # Convert referenced parameter to an integer value
+                count = int(parameters[ref_value])
+                # Remove the Count property from this resource
+                new_template['Resources'][name].pop('Count')
+
+            except AttributeError:
+                # Use numeric count value
+                count = new_template['Resources'][name].pop('Count')
+
+            print("Found 'Count' property with value {} in '{}'"
+                  "resource.... multiplying!".format(count, name))
+            # Remove the original resource from the template
+            # but preserve a local copy of it
+            resourceToMultiply = new_template['Resources'].pop(name)
+            # Create a new block of the resource multiplied with names
+            # ending in the iterator and the placeholders substituted
+            resourcesAfterMultiplication = multiply(name, resourceToMultiply, count)
+            if not set(resourcesAfterMultiplication.keys()) & set(new_template['Resources'].keys()):
+                new_template['Resources'].update(resourcesAfterMultiplication)
+            else:
+                status = 'failed'
+                return status, template
+        else:
+            print("Did not find 'Count' property in"
+                  "'{}' resource.... "
+                  "Nothing to do!".format(name))
+
+    return status, new_template
+
+
+def update_placeholder(resource_structure, iteration):
+    """Update/replace the placeholder in the resource.
+
+    Parameters
+    ----------
+    resource_structure : type
+        CloudFormation resource definition.
+    iteration : integer
+        Number of iteration.
+
+    Returns
+    -------
+    type
+        Modified CloudFormation resource
+
+    """
+    resource_string = json.dumps(resource_structure)
+    place_holder_count = resource_string.count('%s')
+
+    # If the placeholder is found then replace it
+    if place_holder_count > 0:
+
+        print("Found {} occurrences of string placeholder in JSON, replacing with iterator value {}".format(place_holder_count, iteration))
+
+        # Generate a random string for replacement
+        replacement_values = ''.join(choice(ascii_lowercase) for i in range(10))
+
+        # Replace the placeholders
+        resource_string = resource_string % (replacement_values)
+
+        # Convert the string back to json and return it
+        return json.loads(resource_string)
+
+    else:
+
+        print("No occurences of decimal placeholder found in JSON, therefore nothing will be replaced")
+        return resource_structure
+
+
+def multiply(resource_name, resource_structure, count):
+    """Multiply resources and create copies of them.
+
+    Parameters
+    ----------
+    resource_name : string
+        The resource name.
+    resource_structure : JSON
+        CloudFormation resource definition.
+    count : Integer
+        Amount of copies thar are goint to be created.
+
+    Returns
+    -------
+    type
+        Dictonary with the replicated resources.
+
+    """
+    resources = {}
+    # Loop according to the number of times we want to
+    # multiply, creating a new resource each time
+    for iteration in range(1, (count + 1)):
+        print("Multiplying '{}', iteration count {}".format(resource_name,iteration))
+        multipliedResourceStructure = update_placeholder(resource_structure,iteration)
+        resources[resource_name+str(iteration)] = multipliedResourceStructure
+    return resources
+
+
+def handler(event, context):
+    result = process_template(event['fragment'],event['templateParameterValues'])
+    return {
+        'requestId': event['requestId'],
+        'status': result[0],
+        'fragment': result[1],
+    }
